007e35d60677bf2199894036c06278f3808d965a
Jahra <mohammedjahra@gmail.com>
2022-03-19 16:49:11 +0530

    "changes in setting"

--- login/login/settings.py
+++ login/login/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL ='/accounts/dashboard/'
 
