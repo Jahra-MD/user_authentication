1b3bb1bc10397b5c9d962e872fb0185b28f0eb0a
Jahra <mohammedjahra@gmail.com>
2022-03-20 10:39:31 +0530

    "change"

--- login/login/settings.py
+++ login/login/settings.py
@@ -126,10 +126,6 @@ LOGIN_REDIRECT_URL ='/accounts/dashboard/'
 STATIC_ROOT =os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
